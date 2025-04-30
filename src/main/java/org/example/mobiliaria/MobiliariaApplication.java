4ffd3220a4d1efb827e63e875da5a0c899e1cb89
licantropia <110132164+gcorona72@users.noreply.github.com>
2025-04-30 17:59:53 +0200

    Initial commit

---
+++ src/main/java/org/example/mobiliaria/MobiliariaApplication.java
@@ -0,0 +1,13 @@
+package org.example.mobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiliariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobiliariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mobiliaria/MobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobiliariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
